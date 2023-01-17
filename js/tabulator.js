0c0d9e59d98dd8b6712744a8914aa729163b58cc
davidlehub <david.le360@gmail.com>
2023-01-16 19:12:39 -0500

    ..

--- js/tabulator.js
+++ js/tabulator.js
@@ -20,16 +20,23 @@
 // });
 
 // fetch('path/to/data.json')
-fetch('data.json')
+fetch('/data/utterances_withoutID.json')
     .then(response => response.json())
     .then(jsonData => {
     var table = new Tabulator("#example-table", {
+				height:"211px",
+
         data: jsonData,
-				columns: [
-						{title:"Name", field:"name", width:150},
-						{title:"Age", field:"age", align:"left", formatter:"progress"},
-						{title:"Favourite Color", field:"col"},
-						{title:"Date Of Birth", field:"dob", sorter:"date", align:"center"},
+				// responsiveLayout:"hide",  //hide columns that dont fit on the table
+				layout:"fitDataFill", //id020230115231856
+				responsiveLayout:"collapse", //id020230115231856
+				columns:[                 //define the table columns
+					{formatter:"responsiveCollapse", width:30, minWidth:30, hozAlign:"center", resizable:false, headerSort:false}, //id020230115231856
+
+					{title:"Id", field:"id"},
+					{title:"Utterance", field:"utterance", editor:"input"},
+					{title:"Note", field:"note", width:230, editor:"input"},
+					{title:"Used For", field:"usedFor", width:130},
 				],				
         //additional tabulator configuration options
     });
