0e32fc9642640081231550e435caf4a611bdade1
davidlehub <david.le360@gmail.com>
2023-01-16 20:58:56 -0500

    :) (better naming variable for each related table)

--- js/keyboardKeys_tabulatory.js
+++ js/keyboardKeys_tabulatory.js
@@ -1,6 +1,6 @@
-var jsonFileData = "/data/KeyboardKeys_withoutID.json";
-var pageElementID2 = "#keyboardKeys-table";
-var tabulatorConfig = {
+var keyboardKeys_dataFile = "/data/KeyboardKeys_withoutID.json";
+var keyboardKeys_pageElementID2 = "#keyboardKeys-table";
+var tabulator_keyboardKeys_config = {
 	height:"211px",
 
 	// responsiveLayout:"hide",  //hide columns that dont fit on the table
@@ -19,15 +19,43 @@ var tabulatorConfig = {
 		{title:"Tag", field:"tag"},
 		{title:"Id", field:"id"},
 	],	
-				
+
+	rowFormatter:function(row){
+		//create and style holder elements
+	 var holderEl = document.createElement("div");
+	 var tableEl = document.createElement("div");
+
+	 holderEl.style.boxSizing = "border-box";
+	 holderEl.style.padding = "10px 30px 10px 10px";
+	 holderEl.style.borderTop = "1px solid #333";
+	 holderEl.style.borderBotom = "1px solid #333";
+	 
+
+	 tableEl.style.border = "1px solid #333";
+
+	 holderEl.appendChild(tableEl);
+
+	 row.getElement().appendChild(holderEl);
+
+	 var subTable = new Tabulator(tableEl, {
+			 layout:"fitColumns",
+			 data:row.getData().serviceHistory,
+			 columns:[
+			 {title:"Date", field:"date", sorter:"date"},
+			 {title:"Engineer", field:"engineer"},
+			 {title:"Action", field:"actions"},
+			 ]
+	 })
+	},		
+
 	//additional tabulator configuration options
 };
 
 // fetch('path/to/data.json')
-fetch(jsonFileData)
+fetch(keyboardKeys_dataFile)
 	.then(response => response.json())
 	.then(jsonData => {
-	tabulatorConfig["data"] = jsonData;
-	var table = new Tabulator(pageElementID2, tabulatorConfig);
+	tabulator_keyboardKeys_config["data"] = jsonData;
+	var table = new Tabulator(keyboardKeys_pageElementID2, tabulator_keyboardKeys_config);
 	// new Tabulator(pageElementID2, tabulatorConfig); //__itWorksAlso
 });
--- js/utterances_tabulator.js
+++ js/utterances_tabulator.js
@@ -1,28 +1,36 @@
-var jsonFileData = "/data/utterances_withoutID.json"
-var pageElementID = "#utterances-table"
+var utterance_dataFile = "/data/utterances_withoutID.json"
+var utterances_pageElementID2 = "#utterances-table"
+
+// tabulator_keyboardKeys_config = {
+// const tabulator_utterances_config = {
+var tabulator_utterances_config = {
+	height:"211px",
+
+	// responsiveLayout:"hide",  //hide columns that dont fit on the table
+	layout:"fitDataFill", //id020230115231856
+	responsiveLayout:"collapse", //id020230115231856
+	columns:[                 //define the table columns
+		{formatter:"responsiveCollapse", width:30, minWidth:30, hozAlign:"center", resizable:false, headerSort:false}, //id020230115231856
+
+		{title:"Utterance", field:"utterance", editor:"input"},
+		{title:"Note", field:"note", width:230, editor:"input"},
+		{title:"Used For", field:"usedFor", width:130},
+		{title:"Id", field:"id"},
+	],	
+				
+	// data: jsonData,
+
+	//additional tabulator configuration options
+};		
+			
 
 // fetch('path/to/data.json')
-fetch(jsonFileData)
+fetch(utterance_dataFile)
     .then(response => response.json())
     .then(jsonData => {
-    var table = new Tabulator(pageElementID, {
-				height:"211px",
-
-				// responsiveLayout:"hide",  //hide columns that dont fit on the table
-				layout:"fitDataFill", //id020230115231856
-				responsiveLayout:"collapse", //id020230115231856
-				columns:[                 //define the table columns
-					{formatter:"responsiveCollapse", width:30, minWidth:30, hozAlign:"center", resizable:false, headerSort:false}, //id020230115231856
-
-					{title:"Id", field:"id"},
-					{title:"Utterance", field:"utterance", editor:"input"},
-					{title:"Note", field:"note", width:230, editor:"input"},
-					{title:"Used For", field:"usedFor", width:130},
-				],	
-							
-        data: jsonData,
-
-        //additional tabulator configuration options
-    });
+		tabulator_utterances_config["data"] = jsonData;
+			// tabulator_keyboardKeys_config["data"] = jsonData;
+		var table = new Tabulator(utterances_pageElementID2, tabulator_utterances_config);
+		// var table = new Tabulator(pageElementID, tabulator_keyboardKeys_config);
 });
 
