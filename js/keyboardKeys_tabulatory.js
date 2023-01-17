546247428f0f8a4c025ee8adb981fc39c056c5e6
davidlehub <david.le360@gmail.com>
2023-01-17 07:27:04 -0500

    (restructuring codes)

--- js/keyboardKeys_tabulatory.js
+++ js/keyboardKeys_tabulatory.js
@@ -10,10 +10,7 @@ var tabulator_keyboardKeys_config = {
 		{formatter:"responsiveCollapse", width:30, minWidth:30, hozAlign:"center", resizable:false, headerSort:false}, //id020230115231856
 
 		{title:"KeySymbol", field:"keySymbol"},
-		{title:"Utterance", field:"utterance", editor:"input", serviceHistory:[
-			{date:"01/02/2016", engineer:"Steve Boberson", actions:"Changed oli filter"},
-			{date:"07/02/2017", engineer:"Martin Stevenson", actions:"Break light broken"},
-		]},
+		{title:"Utterance", field:"utterance", editor:"input"},
 		{title:"Note", field:"note", width:230, editor:"input"},
 		{title:"KeyName", field:"keyName"},
 		{title:"Tag", field:"tag"},
@@ -37,25 +34,62 @@ var tabulator_keyboardKeys_config = {
 
 	 row.getElement().appendChild(holderEl);
 
-	 var subTable = new Tabulator(tableEl, {
-			 layout:"fitColumns",
-			 data:row.getData().serviceHistory,
-			 columns:[
-			 {title:"Date", field:"date", sorter:"date"},
-			 {title:"Engineer", field:"engineer"},
-			 {title:"Action", field:"actions"},
-			 ]
-	 })
-	},		
+	 var subTable = new Tabulator(tableEl, tabulator_utterances_config)
+
+		//  var subTable = new Tabulator(tableEl, {
+		// 		 layout:"fitColumns",
+		// 		 data:row.getData().serviceHistory,
+		// 		 columns:[
+		// 		 {title:"Date", field:"date", sorter:"date"},
+		// 		 {title:"Engineer", field:"engineer"},
+		// 		 {title:"Action", field:"actions"},
+		// 		 ]
+		//  })
+	},	
 
 	//additional tabulator configuration options
 };
 
-// fetch('path/to/data.json')
-fetch(keyboardKeys_dataFile)
-	.then(response => response.json())
-	.then(jsonData => {
-	tabulator_keyboardKeys_config["data"] = jsonData;
+async function getUsers() {
+	let url = '/data/KeyboardKeys_withoutID.json';
+	try {
+			let res = await fetch(url);
+			return await res.json();
+	} catch (error) {
+			console.log(error);
+	}
+};
+
+
+async function renderUsers(keyboardKeys_pageElementID2, tabulator_keyboardKeys_config) {
+	let users = await getUsers();
+	let html = '';
+
+	console.log(users);
+	// return users;
+
+
+	tabulator_keyboardKeys_config["data"] = users;
+	console.log(tabulator_keyboardKeys_config);
+
+	// return tabulator_keyboardKeys_config; //It return a promise :(
+
+
 	var table = new Tabulator(keyboardKeys_pageElementID2, tabulator_keyboardKeys_config);
-	// new Tabulator(pageElementID2, tabulatorConfig); //__itWorksAlso
-});
+
+	table.on("rowClick", function (e, row) {
+		alert("Row " + row.getIndex() + " Clicked!!!!");
+	});	
+}
+
+renderUsers(keyboardKeys_pageElementID2, tabulator_keyboardKeys_config);
+// console.log(renderUsers(keyboardKeys_pageElementID2, tabulator_keyboardKeys_config));
+
+
+
+// tabulator_keyboardKeys_config["data"] = a1;
+// var table = new Tabulator(keyboardKeys_pageElementID2, tabulator_keyboardKeys_config);
+
+// table.on("rowClick", function (e, row) {
+// 	alert("Row " + row.getIndex() + " Clicked!!!!");
+// });
